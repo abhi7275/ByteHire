e09af85e78120522c635e2c3cd2e16abc3efd3f0
abhi7275 <42292137+abhi7275@users.noreply.github.com>
2023-10-17 11:37:31 +0000

    added dynamic pages for jobs

--- app.py
+++ app.py
@@ -1,39 +1,7 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
-
-# JOBS = [
-#   {
-#     'id': 1,
-#     'title': 'System Engineer',
-#     'location': 'Delhi, NCR',
-#     'salary': 'Rs 4,50,000',
-#     'company': 'Tata Consultancy Services',
-#   },
-#     {
-#     'id':2,
-#     'title': 'Associate System Engineer',
-#     'location': 'Delhi, NCR',
-#     'salary': 'Rs 3,50,000',
-#     'company': 'Tata Consultancy Services',
-#   },
-#   {
-#     'id': 3,
-#     'title': 'Developer',
-#     'location': 'Pune',
-#     'salary': 'Rs 7,50,000',
-#     'company': 'Tata Consultancy Services',
-#   },
-#   {
-#     'id': 4,
-#     'title': 'IT Analyst',
-#     'location': 'Remote, India',
-#     'company': 'Tata Consultancy Services',
-#   }
-# ]
-
-      
 
 @app.route("/")
 def hello_bytehire():
@@ -44,6 +12,14 @@ def hello_bytehire():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
